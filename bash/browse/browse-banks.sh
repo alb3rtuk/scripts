7495863a0dca797ebd2de9bc12f7c4a338e9cf6b
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-16 23:31:02 +0000

    More shell script modifications.

--- bash/browse/browse-banks.sh
+++ bash/browse/browse-banks.sh
@@ -4,6 +4,8 @@
 # Not passing parameter opens all, passing parameter opens a specific account only.
 # Available parameters are: halifax, lloyds, natwest, barclaycard, capitalone
 
-. ~/Repos/Scripts/bash/common/common-detect-displays.sh
+. ~/Repos/Scripts/bash/common/common-utilities.sh
+
+detectDisplays
 
 ruby ~/Repos/Scripts/ruby/browse/browse-banks.rb $displays $1
