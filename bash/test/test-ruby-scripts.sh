b238869fef9a733183b00a5456b2f74882fb1add
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-17 00:10:52 +0000

    Updated test initializer shell script.

--- bash/test/test-ruby-scripts.sh
+++ bash/test/test-ruby-scripts.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
+# Runs all ruby tests in ~/Repos/Scripts/ruby/tests/.
+
 input=$1
 
-echo "$input"
-
-clear
-
 if [ "$input" != '' ]; then
-
-    echo -e "Running: \x1B[92m $input\x1B[0m"
+    echo "Running: \x1B[92m$input\x1B[0m"
     ruby $input
-
 else
-
     cd ~/Repos/Scripts/ruby/tests/
     for i in $(find . -type f -name "*.rb"); do
-        echo -e "Running: \x1B[92m $i\x1B[0m"
+        echo "Running: \x1B[92m$i\x1B[0m"
         ruby $i
     done
-
 fi
