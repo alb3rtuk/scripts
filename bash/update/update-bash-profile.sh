dfc59cdf1c274f9003e7a281352b93a0fd3a02c1
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-17 00:05:15 +0000

    Refactored echo to use common message function.

--- bash/update/update-bash-profile.sh
+++ bash/update/update-bash-profile.sh
@@ -3,5 +3,8 @@
 # Updates ~/.bash_profile with the bash_profile file found in this repo.
 # Scripts/backup/.bash_profile
 
+. ~/Repos/Scripts/bash/common/common-utilities.sh
+
 cp ~/Repos/Scripts/backup/.bash_profile ~/.bash_profile
-echo "`tput setab 4` SUCCESS `tput setab 0` Bash Profile has been updated. Don't forget to run $ `tput setab 3` . ~/.bash_profile `tput setab 0` or $ `tput setab 3` sb `tput setab 0`"
+
+message green "SUCCESS" "Bash Profile has been updated. Don't forget to run $ `tput setab 3` . ~/.bash_profile `tput setab 0` or $ `tput setab 3` sb `tput setab 0`"
