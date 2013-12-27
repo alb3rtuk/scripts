8670031cd3415cffca31b98eff9e3f3511e3d7e2
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-27 20:43:59 +0000

    Added lloyds to getAllBalances script.

--- ruby/banking/get-all-balances.rb
+++ ruby/banking/get-all-balances.rb
@@ -25,6 +25,15 @@ capitalOne = BankCapitalOne.new(
     true
 )
 
+lloyds = BankLloyds.new(
+    crypter.decrypt(LloydsUsername),
+    crypter.decrypt(LloydsPassword),
+    crypter.decrypt(LloydsSecurity),
+    'single',
+    true,
+    true
+)
+
 natWest = BankNatWest.new(
     crypter.decrypt(NatWestUsername),
     crypter.decrypt(NatWestSecurityTop),
@@ -40,6 +49,9 @@ barclayCardBalances = barclayCardBalances[1]
 puts "\n"
 capitalOneBalances = capitalOne.getBalances(true)
 capitalOneBalances = capitalOneBalances[1]
+puts "\n"
+lloydsBalances = lloyds.getBalances(true)
+lloydsBalances = lloydsBalances[1]
 puts "\n"
 natWestBalances = natWest.getBalances(true)
 natWestBalances = natWestBalances[1]
