48667435a84710dc309f451294bf1938d5309e9f
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-27 14:30:13 +0000

    Updated color of title and re-factored the way currency displays.

--- ruby/banking/get-barclaycard-balances.rb
+++ ruby/banking/get-barclaycard-balances.rb
@@ -19,7 +19,7 @@ puts "\n"
 data = barclayCard.getBalances()
 data = data[1]
 
-puts "\n [ #{Rainbow("BarclayCard").foreground('#00ffba')} ]"
+puts "\n [ #{Rainbow("BarclayCard").foreground('#ff008a')} ]"
 
 table(:border => true) do
     row do
@@ -31,11 +31,11 @@ table(:border => true) do
         column('Payment Due', :width => 20, :align => 'right')
     end
     row do
-        column("-£#{'%.2f' % data['current_balance']}", :color => 'red')
-        column("£#{'%.2f' % data['pending_transactions']}", :color => 'green')
-        column("£#{'%.2f' % data['available_credit']}", :color => 'green')
-        column("£#{'%.2f' % data['credit_limit']}", :color => 'green')
-        column("£#{'%.2f' % data['minimum_payment']}", :color => 'green')
+        column("-£#{toCurrency(data['current_balance'])}", :color => 'red')
+        column("£#{toCurrency(data['pending_transactions'])}", :color => 'green')
+        column("£#{toCurrency(data['available_credit'])}", :color => 'green')
+        column("£#{toCurrency(data['credit_limit'])}", :color => 'green')
+        column("£#{toCurrency(data['minimum_payment'])}", :color => 'green')
         column("#{data['due_date'].strftime('%d %b %Y')}", :color => 'white')
     end
 end
