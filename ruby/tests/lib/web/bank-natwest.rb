eb41aac6fd995cac7976d3605700cf84bf1468b4
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-27 20:43:39 +0000

    Code cleanup.

--- ruby/tests/lib/web/bank-natwest.rb
+++ ruby/tests/lib/web/bank-natwest.rb
@@ -22,15 +22,13 @@ class TestBankNatWest < Test::Unit::TestCase
         data = natWest.getBalances
         puts "\n"
 
-        browser = data[0]
-        balances = data[1]
-
         f = 'ctl00_secframe';
-        assert_equal(browser.frame(:id => f).tr(:id => 'Account_A412AD6062AE989A9FCDAEB7D9ED8A594808AC87').exists?, true)
-        assert_equal(browser.frame(:id => f).tr(:id => 'Account_CE99D6FF6219B59BB28B6A42825D98D60B92326C').exists?, true)
-        assert_equal(browser.frame(:id => f).tr(:id => 'Account_FAB7EFB59260BED0F1081E761570BF4227C37E6B').exists?, true)
 
-        balances.each { |key, value|
+        assert_equal(data[0].frame(:id => f).tr(:id => 'Account_A412AD6062AE989A9FCDAEB7D9ED8A594808AC87').exists?, true)
+        assert_equal(data[0].frame(:id => f).tr(:id => 'Account_CE99D6FF6219B59BB28B6A42825D98D60B92326C').exists?, true)
+        assert_equal(data[0].frame(:id => f).tr(:id => 'Account_FAB7EFB59260BED0F1081E761570BF4227C37E6B').exists?, true)
+
+        data[1].each { |key, value|
             assert_equal(key.is_a?(String), true)
             assert_equal(value.is_a?(Float), true)
         }
