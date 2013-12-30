a2f193452c1c9d5464bd5195c31514d2eb5f85ec
Albert Rannetsperger <alb3rtuk@me.com>
2013-12-30 22:50:13 +0000

    Fixed spacing in test runner script.

--- bash/tests/run-ruby-tests.sh
+++ bash/tests/run-ruby-tests.sh
@@ -7,12 +7,17 @@
 input=$1
 
 if [ "$input" != '' ]; then
+    echo
     message green "Running" "$input"
+    echo
     ruby $input
+    echo
 else
     cd ~/Repos/Scripts/ruby/tests/
     for input in $(find . -type f -name "*.rb"); do
+        echo
         message green "Running" "$input"
+        echo
         ruby $input
     done
 fi
