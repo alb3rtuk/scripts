856f00e8333e3b28af3f4d9a598682855f4c0b55
Albert Rannetsperger <alb3rtuk@me.com>
2014-01-02 15:12:32 +0000

    Reverted ruby sandbox back to being blank.. as it should be :)

--- ruby/quick-commands/rs.rb
+++ ruby/quick-commands/rs.rb
@@ -1,6 +1,1 @@
 require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
-require '/Users/Albert/Repos/Scripts/ruby/lib/web/bank-barclaycard.rb'
-
-barclayCard = BankBarclayCard.new(Encrypter.new.decrypt(BarclayCardUsername), Encrypter.new.decrypt(BarclayCardPin), Encrypter.new.decrypt(BarclayCardSecurity), 'multiple')
-
-barclayCard.payBarclayCard('5.00', 'halifax')
