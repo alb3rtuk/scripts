57f47df592be26788a87a9de7702327b6f3ef6e0
Albert Rannetsperger <alb3rtuk@me.com>
2014-01-06 14:45:31 +0000

    Added 'FLUSH DNS CACHE' to update-apache script.

--- bash/sys/update-apache.sh
+++ bash/sys/update-apache.sh
@@ -10,5 +10,7 @@ sudo cp ~/Repos/Scripts/backup/php.ini /private/etc/php.ini
 message green "SUCCESS" "Updated: \033[33m/private/etc/php.ini\033[0m"
 message magenta "SUCCESS" "Restarted Apache"
 sudo apachectl restart
+message magenta "SUCCESS" "Flushed DNS Cache"
+dscacheutil -flushcache
 
 exit
