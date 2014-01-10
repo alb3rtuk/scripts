b0a3706a8fec7156210c69997cb0fe6e27cdb540
Albert Rannetsperger <alb3rtuk@me.com>
2014-01-09 15:35:27 -1000

    Added /etc/hosts file to update-apache script.

--- bash/sys/update-apache.sh
+++ bash/sys/update-apache.sh
@@ -4,15 +4,23 @@
 
 sudo cp ~/Repos/Scripts/backup/hosts /private/etc/hosts
 message green "SUCCESS" "Updated: \033[33m/private/etc/hosts\033[0m"
+
 sudo cp ~/Repos/Scripts/backup/httpd.conf /private/etc/apache2/httpd.conf
 message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/httpd.conf\033[0m"
+
 sudo cp ~/Repos/Scripts/backup/httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf
 message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/extra/httpd-vhosts.conf\033[0m"
-sudo cp ~/Repos/Scripts/backup/php.ini /private/etc/php.ini
-message green "SUCCESS" "Updated: \033[33m/private/etc/php.ini\033[0m"
-message magenta "SUCCESS" "Restarted Apache"
+
+sudo cp ~/Repos/Scripts/backup/httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf
+message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/extra/httpd-vhosts.conf\033[0m"
+
+sudo cp ~/Repos/Scripts/backup/hosts /etc/hosts
+message green "SUCCESS" "Updated: \033[33m/etc/hosts\033[0m"
+
 sudo apachectl restart
-message magenta "SUCCESS" "Flushed DNS Cache"
+message magenta "SUCCESS" "Restarted Apache"
+
 dscacheutil -flushcache
+message magenta "SUCCESS" "Flushed DNS Cache"
 
 exit
