c70baa6cae0d63f387f886cb6c20e29d3e2c30f5
Albert Rannetsperger <alb3rtuk@me.com>
2014-01-09 15:28:10 -1000

    Swapped the order of the terminal output messages.

--- bash/sys/update-apache.sh
+++ bash/sys/update-apache.sh
@@ -5,9 +5,9 @@
 sudo cp ~/Repos/Scripts/backup/hosts /private/etc/hosts
 message green "SUCCESS" "Updated: \033[33m/private/etc/hosts\033[0m"
 sudo cp ~/Repos/Scripts/backup/httpd.conf /private/etc/apache2/httpd.conf
-message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/extra/httpd-vhosts.conf\033[0m"
-sudo cp ~/Repos/Scripts/backup/httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf
 message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/httpd.conf\033[0m"
+sudo cp ~/Repos/Scripts/backup/httpd-vhosts.conf /private/etc/apache2/extra/httpd-vhosts.conf
+message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/extra/httpd-vhosts.conf\033[0m"
 sudo cp ~/Repos/Scripts/backup/php.ini /private/etc/php.ini
 message green "SUCCESS" "Updated: \033[33m/private/etc/php.ini\033[0m"
 message magenta "SUCCESS" "Restarted Apache"
