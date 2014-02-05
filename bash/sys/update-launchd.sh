b2140d1d2ff04c2cea9882bc62355cd4a55cf5cb
Albert Rannetsperger <alb3rtuk@me.com>
2014-02-05 07:46:59 +0000

    Code cleanup.

--- bash/sys/update-launchd.sh
+++ bash/sys/update-launchd.sh
@@ -10,23 +10,22 @@ launchdDirectory="/Users/Albert/Library/LaunchAgents/"
 
 # First, remove all launchd jobs and symbolic links from LaunchAgents directory.
 cd ${launchdDirectory}
-for input in $(ls -lR $launchdDirectory | grep ^l); do
-    if [[ $input == albert.*.plist ]]; then
-        rm $input
+for input in $(ls -lR ${launchdDirectory} | grep ^l); do
+    if [[ ${input} == albert.*.plist ]]; then
+        rm ${input}
         inputLength=${#input}
-        inputLength=`expr $inputLength - 6`
-        launchctl remove ${input:0:$inputLength}
-        # message red "Removed Launchd Job" "${input:0:$inputLength}"
+        inputLength=`expr ${inputLength} - 6`
+        launchctl remove ${input:0:${inputLength}}
     fi
 done
 
 # Second, recreate all launchd jobs from the files in the git repo.
 cd ${plistDirectory}
 for input in $(find . -type f -name "albert.*.plist"); do
-    ln -s $plistDirectory${input:2} $launchdDirectory${input:2}
-    launchctl load $launchdDirectory${input:2}
+    ln -s ${plistDirectory}${input:2} ${launchdDirectory}${input:2}
+    launchctl load ${launchdDirectory}${input:2}
     inputLength=${#input}
-    inputLength=`expr $inputLength - 8`
+    inputLength=`expr ${inputLength} - 8`
     message green "REGISTERED LAUNCHD" "`tput setaf 3`${input:2:$inputLength}"
 done
 exit
