0aae74cc5dca8d2937b0a96b39246657aa91ab07
Albert Rannetsperger <alb3rtuk@me.com>
2014-02-05 08:43:53 +0000

    Deleted cron testing stuff.

--- bash/cron/cron-logger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. ~/Repos/Scripts/bash/common/common-utilities.sh
-
-logCron "Cron ran successfully."
-exit
--- bash/launchd/albert.daemon.logger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. ~/Repos/Scripts/bash/common/common-utilities.sh
-
-logCron "LaunchDaemon ran successfully."
-exit
