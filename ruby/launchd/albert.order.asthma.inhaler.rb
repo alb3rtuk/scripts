1481f371d4ab4a528618ea5e9b88ff0d38a7824c
Albert Rannetsperger <alb3rtuk@me.com>
2014-02-05 08:32:50 +0000

    WOOOHOOO!!! Launchd is finally finding phantomjs!! For future ref: Needed to add 'setenv PATH /usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin' to '/etc/launchd.conf'.

--- ruby/launchd/albert.order.asthma.inhaler.rb
+++ ruby/launchd/albert.order.asthma.inhaler.rb
@@ -1,13 +1,12 @@
 require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
 require '/Users/Albert/Repos/Scripts/ruby/lib/encrypter.rb'
-require '/Users/Albert/Repos/Scripts/ruby/lib/web/bank-capitalone.rb'
+require '/Users/Albert/Repos/Scripts/ruby/lib/web/nevil-road-surgery.rb'
 
-capitalOne = BankCapitalOne.new(
-    Encrypter.new.decrypt(CapitalOneUsername),
-    Encrypter.new.decrypt(CapitalOneSecurity),
-    'single',
-    true
+nrs = NevilRoadSurgery.new(
+    Encrypter.new.decrypt(NevilRoadUsername),
+    Encrypter.new.decrypt(NevilRoadPassword)
 )
 
-capitalOne.login()
+nrs.orderAsthma('single', true)
+
 exit
