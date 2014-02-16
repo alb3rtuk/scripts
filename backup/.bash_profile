d33b9bf9e01232b7e975e9e9b6b3663f6c2b61e6
Albert Rannetsperger <alb3rtuk@me.com>
2014-02-16 15:42:57 +0000

    Created quick script to download, create & install cookbooks to the chef server. Also commits to git. MASSIVE TIME SAVER!

--- backup/.bash_profile
+++ backup/.bash_profile
@@ -49,6 +49,9 @@ alias pay-barclaycard='~/Repos/Scripts/bash/banking/pay-barclaycard.sh'
 alias pay-capitalone='~/Repos/Scripts/bash/banking/pay-capitalone.sh'
 alias pay-lloyds='~/Repos/Scripts/bash/banking/pay-lloyds.sh'
 
+### CHEF #####################
+alias knife-get-cookbook='~/Repos/Scripts/bash/chef/knife-get-cookbook.sh'
+
 ### EC2 ######################
 alias ec2-create='~/Repos/Scripts/bash/ec2/ec2-create.sh'
 alias ec2-delete='~/Repos/Scripts/bash/ec2/ec2-delete.sh'
---
+++ bash/chef/knife-get-cookbook.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. ~/Repos/Scripts/bash/common/utilities.sh
+
+
+cookbook=$1
+
+if [[ $cookbook == "" ]]; then
+    message red "ERROR" "No cookbook (1st parameter) has been defined."
+    exit
+fi
+
+cd ~/Repos/Chef/
+
+mkdir tmp
+cd tmp
+knife cookbook site download ${cookbook}
+
+for file in ~/Repos/Chef/tmp/*
+do
+    tar xzvf ${file} -C ~/Repos/Chef/cookbooks/
+done
+
+cd ~/Repos/Chef/
+rm -rf tmp/
+
+git add cookbooks/${cookbook}
+git commit -m "Added cookbook: $cookbook"
+git push
+
+knife cookbook upload ${cookbook}
+
+exit
