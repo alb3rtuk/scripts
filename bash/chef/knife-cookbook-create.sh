83c0b23e62ac50b630a6b230a664eab5c0e9ce90
Albert Rannetsperger <alb3rtuk@me.com>
2014-02-18 16:13:37 +0000

    Chef create + delete cookbook scripts now working! WOOP!

---
+++ bash/chef/knife-cookbook-create.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+. ~/Repos/Scripts/bash/common/utilities.sh
+
+cookbook=$1
+
+if [[ ${cookbook} == "" ]]; then
+    message red "ERROR" "You must pass the \033[33mcookbook name\033[0m as the \033[32m1st parameter\033[0m."
+    exit
+fi
+
+if [[ -d ~/Repos/Chef/cookbooks/${cookbook} ]]; then
+    message red "ERROR" "Cookbook with the name: \033[33m${cookbook}\033[0m already exists."
+    exit
+fi
+
+message blue "KNIFE" "Creating cookbook: \033[33m${cookbook}\033[0m"
+
+metadata_file="cookbooks/${cookbook}/metadata.rb"
+default_recipe="cookbooks/${cookbook}/recipes/default.rb"
+
+cd ~/Repos/Chef/
+
+knife cookbook create ${cookbook}
+echo "\033[36mCreated skeleton files\033[0m"
+
+# DELETE THE CHANGELOG & README FILES
+rm -rf cookbooks/${cookbook}/CHANGELOG.md
+rm -rf cookbooks/${cookbook}/README.md
+echo "\033[36mDeleted CHANGELOG.md & README.md\033[0m"
+
+# SETUP METADATA FILE
+echo "name             '${cookbook}'" > ${metadata_file}
+echo "maintainer       'Albert Rannetsperger'" >> ${metadata_file}
+echo "maintainer_email 'alb3rtuk@me.com'" >> ${metadata_file}
+echo "license          'All rights reserved'" >> ${metadata_file}
+echo "description      'Installs/Configures ${cookbook}'" >> ${metadata_file}
+echo "version          '0.1.0'" >> ${metadata_file}
+echo "\033[36mConfigured\033[0m \033[33m${metadata_file}\033[0m"
+
+# SETUP METADATA FILE
+echo "#" > ${default_recipe}
+echo "# Cookbook Name:: ${cookbook}" >> ${default_recipe}
+echo "# Recipe:: default" >> ${default_recipe}
+echo "#" >> ${default_recipe}
+echo "# Copyright 2014, Albert Rannetsperger" >> ${default_recipe}
+echo "#" >> ${default_recipe}
+echo "# All rights reserved - Do Not Redistribute" >> ${default_recipe}
+echo "#" >> ${default_recipe}
+echo "\033[36mConfigured\033[0m \033[33m${default_recipe}\033[0m"
+
+knife cookbook upload ${cookbook}
+echo "Uploaded cookbook to Chef server"
+
+git add cookbooks/${cookbook}
+git commit cookbooks/${cookbook} -m "Created a new cookbook: ${cookbook}"
+git push
+echo "\033[36mCommited & pushed revision to GitHub repo\033[0m"
+
+exit
--- bash/chef/knife-cookbook-delete.sh
+++ bash/chef/knife-cookbook-delete.sh
@@ -5,17 +5,32 @@
 cookbook=$1
 
 if [[ $cookbook == "" ]]; then
-    message red "ERROR" "No cookbook (1st parameter) has been defined."
+    message red "ERROR" "You must pass the \033[33mcookbook name\033[0m as the \033[32m1st parameter\033[0m."
     exit
 fi
 
-cd ~/Repos/Chef/cookbooks/
+if [[ ! -d ~/Repos/Chef/cookbooks/${cookbook} ]]; then
+    message red "ERROR" "The cookbook: \033[33m${cookbook}\033[0m doesn't seem to exist."
+    exit
+fi
+
+read -p "`echo "\`tput setab 1\` WARNING \`tput setab 0\` \033[0mYou are about to permanently delete the cookbook: \033[33m${cookbook}\033[0m. \033[31mThis action cannot be undone!\033[0m Continue? \033[32m[y/n]\033[0m \033[37m=>\033[0m "`" -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "`tput setab 1` Script was aborted! `tput setab 0`"
+    exit
+fi
+
+cd ~/Repos/Chef
 
 knife cookbook delete ${cookbook}
+echo "\033[36mRemoved: \033[33m${cookbook}\033[36m from Chef server\033[0m"
 
-rm -rf ${cookbook}
+rm -rf cookbooks/${cookbook}
+echo "\033[36mDeleted:\033[0m \033[33mcookbooks/${cookbook}\033[0m"
 
-git commit -a -m "Removed cookbook: $cookbook"
+git add cookbooks/${cookbook} --all
+git commit -o cookbooks/${cookbook} -m "Removed cookbook: $cookbook"
 git push
+echo "\033[36mCommited & pushed revision to GitHub repo\033[0m"
 
 exit
