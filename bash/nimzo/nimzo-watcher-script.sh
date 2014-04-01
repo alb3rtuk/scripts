6bea084351f6d529e60eea82413f7bdff7452aed
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-01 11:20:06 -1000

    Watcher script successfully watching for directory changes. Still a work in progress but getting there.

--- bash/nimzo/nimzo-watcher-script.sh
+++ bash/nimzo/nimzo-watcher-script.sh
@@ -4,4 +4,26 @@
 
 cd ~/Repos/Nimzo/httpdocs/public/dev
 
-echo $(date +"%T")
+lastScriptRun=$(date +"%T %Z")
+lastModifiedTime=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
+
+storedModifiedTime=$(cat /tmp/nimzo-watcher-last-change.txt)
+
+echo "Checking for changes within: ~/Repos/Nimzo/httpdocs/public/dev"
+echo
+echo "Last script run:  ${lastScriptRun}"
+
+if [[ ${storedModifiedTime} != ${lastModifiedTime} ]]; then
+    lastModifiedFile=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 2 -d" ")
+    find="./"
+    replace="pubic/"
+    lastModifiedFile=${lastModifiedFile//$find/$replace}
+
+    echo "$(date +"%T") - ${lastModifiedFile}" >> /tmp/nimzo-watcher-log.txt
+
+fi
+
+echo
+tail -10 /tmp/nimzo-watcher-log.txt
+
+echo ${lastModifiedTime} > /tmp/nimzo-watcher-last-change.txt
