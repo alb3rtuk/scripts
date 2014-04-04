6eefe9ff4f989ca6c837cd0cb9d2943c61cb8991
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-03 23:32:51 -1000

    Git pushes now hiding all output.

--- bash/launchd/albert.git.push.repos.sh
+++ bash/launchd/albert.git.push.repos.sh
@@ -3,16 +3,16 @@
 . ~/Repos/Scripts/bash/common/utilities.sh
 
 cd ~/Repos/Nimzo/
-git push
+git push > /dev/null 2>&1
 
 cd ~/Repos/Nimzo-legacy/
-git push
+git push > /dev/null 2>&1
 
 cd ~/Repos/Scripts/
-git push
+git push > /dev/null 2>&1
 
 cd ~/Repos/Sleek/
-git push
+git push > /dev/null 2>&1
 
 logCron "Launchd job: 'albert.git.push.repos' ran successfully. All repos pushed to git."
 
