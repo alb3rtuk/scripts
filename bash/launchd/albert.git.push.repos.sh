19f196d36797ce5fbea3d8c891a218fe664596e2
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-04 11:09:20 -1000

    Removed nimzo-legacy from automatic git-push job.

--- bash/launchd/albert.git.push.repos.sh
+++ bash/launchd/albert.git.push.repos.sh
@@ -5,15 +5,12 @@
 cd ~/Repos/Nimzo/
 git push > /dev/null 2>&1
 
-cd ~/Repos/Nimzo-legacy/
-git push > /dev/null 2>&1
-
 cd ~/Repos/Scripts/
 git push > /dev/null 2>&1
 
 cd ~/Repos/Sleek/
 git push > /dev/null 2>&1
 
-logCron "Launchd job: 'albert.git.push.repos' ran successfully. All repos pushed to git."
+logCron "Launchd job: 'albert.git.push.repos' ran successfully. All repos (Nimzo, Scripts & Sleek) pushed to git."
 
 exit
