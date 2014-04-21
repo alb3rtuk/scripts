5c007daf9bab2ba0d5048609ae55b6044cd2d2ff
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-21 23:54:27 +0100

    Created all blank bash files, updated bash profile (to include system) and started re-factoring the horrible mess that was the create/delete script.

---
+++ bash/nimzo/nimzo-delete-system.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. ~/Repos/Scripts/bash/common/utilities.sh
+. ~/Repos/Scripts/bash/common/nimzo.sh
+
+controllerName=$1
+
+validateControllerName
+
+ruby ~/Repos/Scripts/ruby/nimzo/nimzo-create-delete.rb ${controllerName} system delete
