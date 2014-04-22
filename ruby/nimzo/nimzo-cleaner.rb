e49273903e9b747b746413e81b9f12b72a30e5ae
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 16:40:15 +0100

    Created 2 classes, nimzoCleaner (to clean the directory structure) & nimzoFileMaker (which will make all the controllers/views/tests for the modals/overlays/etc).

---
+++ ruby/nimzo/nimzo-cleaner.rb
@@ -0,0 +1,12 @@
+require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo.rb'
+
+class NimzoCleaner
+
+    def initialise
+
+        puts 'Initialized NimzoCleaner.'
+
+    end
+
+end
---
+++ ruby/nimzo/nimzo-file-maker.rb
@@ -0,0 +1,12 @@
+require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo.rb'
+
+class NimzoFileMaker
+
+    def initialize(files = Array.new)
+
+        puts files.inspect
+
+    end
+
+end
