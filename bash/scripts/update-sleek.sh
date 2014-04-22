6d218a9e1830065fa22d4934e43d7b4cafd8ab1a
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 11:07:09 +0100

    Temporary commit for updating (or more specifically) copying sleek files. This needs work & later needs to be incorporated into the nimzo-watcher script.

--- bash/scripts/update-sleek.sh
+++ bash/scripts/update-sleek.sh
@@ -2,40 +2,18 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
-tmpFile="/tmp/ec2-bash.sh"
 
-message red "UPDATE SLEEK" "Running update-sleek script:"
+cd ~/Repos/Nimzo/httpdocs/private/lib/sleek/
+nimzoSleekPhpLMT=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
 
-# CREATE LOCAL BASH SCRIPT TO RUN ON AMAZON
-touch ${tmpFile}
-echo "cd ~/repos/Sleek" >> ${tmpFile}
-echo "git pull" >> ${tmpFile}
-echo "cd /" >> ${tmpFile}
-echo "sudo su" >> ${tmpFile}
-echo "rm -rf /var/www" >> ${tmpFile}
-echo "cd var" >> ${tmpFile}
-echo "mkdir www" >> ${tmpFile}
-echo "cd /" >> ${tmpFile}
-echo "cp -r /home/ubuntu/repos/Sleek/httpdocs/. /var/www" >> ${tmpFile}
-echo "exit" >> ${tmpFile}
-echo "cd /var/www" >> ${tmpFile}
-echo "ls -la" >> ${tmpFile}
+cd ~/Repos/Nimzo/httpdocs/public/dev/lib/less/sleek/
+nimzoSleekDevLMT=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
 
-cd ~/Repos/Sleek
-pwd=`pwd`
-echo "\033[37mSwitching to: $pwd\033[0m"
+cd ~/Repos/Sleek/httpdocs/
+nimzoSleekLMT=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
 
-echo "\033[37mPushing 'Sleek' repo to GitHub\033[0m"
-git push
+echo $nimzoSleekPhpLMT
+echo $nimzoSleekDevLMT
+echo $nimzoSleekLMT
 
-cd ~/.ssh/
-pwd=`pwd`
-echo "\033[37mSwitching to: $pwd\033[0m"
-
-echo "\033[37mAttempting to SSH into Amazon EC2 instance (ec2-54-201-246-204.us-west-2.compute.amazonaws.com)\033[0m"
-ssh -i ec2-admin.pem ubuntu@ec2-54-201-246-204.us-west-2.compute.amazonaws.com 'bash -s' < ${tmpFile}
-
-rm ${tmpFile}
-
-
-
+exit
