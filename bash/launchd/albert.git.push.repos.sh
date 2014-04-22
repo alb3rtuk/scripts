dbb4ba9f689948191649c4f1c5ae8a2484367eb0
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 11:05:21 +0100

    Removed logging again. Username wasn't set correctly.. doh!

--- bash/launchd/albert.git.push.repos.sh
+++ bash/launchd/albert.git.push.repos.sh
@@ -2,8 +2,6 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
-logCron "Attempting to push Git repos."
-
 cd ~/Repos/Nimzo/
 git push > /dev/null 2>&1
 
@@ -12,7 +10,5 @@ git push > /dev/null 2>&1
 
 cd ~/Repos/Sleek/
 git push > /dev/null 2>&1
-
-logCron "Successfully pushed Git repos."
 
 exit
