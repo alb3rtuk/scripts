edfed7abef73c54b97917375448a8255da64d18c
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 06:20:36 +0100

    Minor improvements to watcher script. Checks that the modifiedTime /tmp file exists, and if not, defaults LMT to null (0).

--- bash/nimzo/nimzo-watcher-script.sh
+++ bash/nimzo/nimzo-watcher-script.sh
@@ -7,7 +7,13 @@ cd ~/Repos/Nimzo/httpdocs/public/dev
 lastScriptRun=$(date +"%T %Z")
 lastModifiedTime=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
 
-storedModifiedTime=$(cat /tmp/nimzo-watcher-last-change.txt)
+storedModifiedFile="/tmp/nimzo-watcher-last-change.txt"
+
+if [[ -f ${storedModifiedFile} ]]; then
+    storedModifiedTime=$(cat ${storedModifiedFile})
+else
+    storedModifiedTime=0
+fi
 
 echo "Checking for changes within: ~/Repos/Nimzo/httpdocs/public/dev"
 echo
@@ -17,9 +23,9 @@ if [[ ${storedModifiedTime} != ${lastModifiedTime} ]]; then
     lastModifiedFile=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 2 -d" ")
     find="./"
     replace="public/"
-    lastModifiedFile=${lastModifiedFile//$find/$replace}
+    lastModifiedFile=${lastModifiedFile//${find}/${replace}}
     echo "$(date +"%T") - ${lastModifiedFile}" >> /tmp/nimzo-watcher-log.txt
-    echo ${lastModifiedTime} > /tmp/nimzo-watcher-last-change.txt
+    echo ${lastModifiedTime} > ${storedModifiedFile}
 fi
 
 tail -30 /tmp/nimzo-watcher-log.txt
@@ -30,4 +36,3 @@ if [[ ${storedModifiedTime} != ${lastModifiedTime} ]]; then
     echo
     ./nimzo-update.sh
 fi
-
