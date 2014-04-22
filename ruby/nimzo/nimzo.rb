3556b15fb58af62a89ddb57017e7ddc0ec8af754
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 16:57:20 +0100

    Updated nimzo common file to include all nimzo-related classes + added constants.

--- ruby/nimzo/nimzo.rb
+++ ruby/nimzo/nimzo.rb
@@ -1,2 +1,14 @@
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo-cleaner.rb'
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo-create-delete.rb'
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo-file-maker.rb'
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo-file-rewriter.rb'
+
 class Nimzo
+
+    PATH_TO_REPO = '/Users/Albert/Repos/Nimzo'
+    PATH_TO_PHP = "#{Nimzo.PATH_TO_REPO}/httpdocs/private/"
+    PATH_TO_DEV = "#{Nimzo.PATH_TO_REPO}/httpdocs/public/dev/"
+    PATH_TO_MIN = "#{Nimzo.PATH_TO_REPO}/httpdocs/public/min/"
+    PATH_TO_TESTS = "#{Nimzo.PATH_TO_REPO}/tests-php/private/"
+
 end
