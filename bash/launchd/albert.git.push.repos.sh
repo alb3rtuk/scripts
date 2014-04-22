fe1c86fb4aaeb6207e91c9af0b050c207ce06783
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 06:25:20 +0100

    Re-added logging to Git Repo pusher because for some strange reason it stopped working.

--- bash/launchd/albert.git.push.repos.sh
+++ bash/launchd/albert.git.push.repos.sh
@@ -2,6 +2,8 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
+logCron "Attempting to push Git repos."
+
 cd ~/Repos/Nimzo/
 git push > /dev/null 2>&1
 
@@ -10,5 +12,7 @@ git push > /dev/null 2>&1
 
 cd ~/Repos/Sleek/
 git push > /dev/null 2>&1
+
+logCron "Successfully pushed Git repos."
 
 exit
