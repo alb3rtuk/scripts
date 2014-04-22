04ad639dd455f0c00a0517ecfe4f46bead3a94a5
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 21:05:57 +0100

    Changed error text to match output of ruby scripts more (in regards to spacing).

--- bash/common/nimzo.sh
+++ bash/common/nimzo.sh
@@ -2,9 +2,12 @@
 
 validateControllerName() {
     if [[ ${controllerName} == "" ]]; then
-        message red "ERROR" "You must specify a name (1st parameter). Valid names are:"
-        echo "\033[33mdashboard\033[0m"
-        echo "\033[33mdashboard/messages\033[0m"
+        echo
+        message red "ERROR" "You must specify a 'route'. Valid routes are:"
+        echo
+        echo "        \033[33mdashboard\033[0m"
+        echo "        \033[33mdashboard/messages\033[0m"
+        echo
         exit
     fi
 }
