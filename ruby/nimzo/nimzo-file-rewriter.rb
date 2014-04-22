387246efc9cfaf8fbe243d944ed835b32f34636a
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-22 16:57:50 +0100

    Created new class: NimzoRewriter. This class will re-write all the reference files.

---
+++ ruby/nimzo/nimzo-file-rewriter.rb
@@ -0,0 +1,12 @@
+require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
+require '/Users/Albert/Repos/Scripts/ruby/nimzo/nimzo.rb'
+
+class NimzoRewriter
+
+    def initialize
+
+        puts 'Initialized NimzoRewriter.'
+
+    end
+
+end
