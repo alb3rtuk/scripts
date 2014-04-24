a2705b8254a4c4c1de88e13ea6071571e665618a
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-24 14:58:34 +0100

    File-maker now getting className + namespace correctly for /lib files.

--- ruby/nimzo/nimzo-file-maker.rb
+++ ruby/nimzo/nimzo-file-maker.rb
@@ -17,16 +17,19 @@ class NimzoFileMaker
     # @param space
     def initialize(type = '', paths = Array.new, files = Array.new, space = '')
 
-        @type = type
+        @type = type.downcase
         @paths = paths
         @files = files
         @space = space
+        @namespace = ''
 
-        @namespace = @type.capitalize
+        if @type != 'lib'
+            @namespace = @type.capitalize
+        end
 
         # Make sure the particular controller type is valid.
         # This error cannot be reached through incorrect user input.
-        unless inArray(%w(app modal overlay system widget), @type)
+        unless inArray(%w(app lib modal overlay system widget), @type)
             puts("\x1B[33m#{@type}\x1B[0m is not a valid controller type. There is an error in your bash script, not your input.")
             exit
         end
@@ -112,7 +115,15 @@ class NimzoFileMaker
             self.createFileView(file, File.dirname(file).sub("#{$PATH_TO_PHP}#{@type}/views/", ''))
             return
         end
-        exitScript('Couldn\'t determine file type.')
+        if file.include? "#{$PATH_TO_PHP}lib/"
+            self.createFileLib(file)
+            return
+        end
+        if file.include? "#{$PATH_TO_TESTS}lib/"
+            self.createFileLibTest(file)
+            return
+        end
+        exitScript("Couldn't determine file type. Please note that the file '\x1B[33m#{file}\x1B[0m' has already been created and will need to be deleted manually.")
     end
 
     # Creates the Controller.
@@ -229,7 +240,84 @@ class NimzoFileMaker
     end
 
     # Creates the View.
-    # @param file
+    # @param filename
+    # @param route
     def createFileView(filename, route)
+    end
+
+    # Create file within /lib
+    # @param filename
+    def createFileLib(filename)
+        className, namespace = getLibFileData(filename)
+
+        puts className
+        puts namespace
+        exit
+
+        #
+        #File.open(filename, 'w') { |file|
+        #    file.puts '<?php'
+        #    file.puts ''
+        #    unless namespace.nil?
+        #        file.puts "namespace #{namespace};"
+        #        file.puts ''
+        #        file.puts 'use PHPUnit_Framework_TestCase;'
+        #        file.puts ''
+        #    end
+        #    file.puts '/**'
+        #    file.puts " * @group #{group}"
+        #    route.split('/').each { |routeParameter|
+        #        routeParameter[0] = routeParameter.upcase[0..0]
+        #        group = "#{group}/#{routeParameter}"
+        #        file.puts " * @group #{group}"
+        #    }
+        #    file.puts " * @package #{@namespace}"
+        #    file.puts ' */'
+        #    file.puts "class #{className} extends PHPUnit_Framework_TestCase"
+        #    file.puts '{'
+        #    file.puts '    /**'
+        #    file.puts '     * @return void'
+        #    file.puts '     */'
+        #    file.puts '    public function setUp()'
+        #    file.puts '    {'
+        #    file.puts '    }'
+        #    file.puts ''
+        #    file.puts '    /**'
+        #    file.puts '     * @return void'
+        #    file.puts '     */'
+        #    file.puts '    public function tearDown()'
+        #    file.puts '    {'
+        #    file.puts '    }'
+        #    file.puts ''
+        #    file.puts '    /**'
+        #    file.puts '     * @return void'
+        #    file.puts '     */'
+        #    file.puts '    public function testSomething()'
+        #    file.puts '    {'
+        #    file.puts '        $this->assertTrue(true);'
+        #    file.puts '    }'
+        #    file.write '}'
+        #}
+    end
+
+    # Extracts namespace + className from lib filename. Used internally.
+    # @param filename
+    def getLibFileData(filename)
+        fileSplit = filename.sub("#{$PATH_TO_PHP}lib/", '')
+        fileSplit = fileSplit.split('/')
+        if fileSplit[0].downcase != 'core'
+            namespace = fileSplit[0].capitalize
+        else
+            namespace = nil
+        end
+        className = File.basename(fileSplit[1], File.extname(fileSplit[1]))
+        className[0] = className.upcase[0..0]
+        return className, namespace
+    end
+
+    # Create test for file within /lib
+    # @param filename
+    def createFileLibTest(filename)
+        puts filename
     end
 end
