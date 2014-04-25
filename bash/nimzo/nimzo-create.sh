2960b010ac5b220d34b267c317bdeffb90619b4e
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-25 10:31:01 +0100

    Moved common Nimzo shell script into the /nimzo directory.

--- bash/nimzo/nimzo-create.sh
+++ bash/nimzo/nimzo-create.sh
@@ -4,7 +4,7 @@ type=$1
 route=$2
 
 . ~/Repos/Scripts/bash/common/utilities.sh
-. ~/Repos/Scripts/bash/common/nimzo.sh
+. ~/Repos/Scripts/bash/nimzo/common/nimzo.sh
 
 # Continue to ruby..
 ruby ~/Repos/Scripts/ruby/nimzo/nimzo-create-remove.rb ${type} ${route} create
