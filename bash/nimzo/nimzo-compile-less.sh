d8dbefefc7ebba42692a69f9757f593a6343ef26
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-26 13:11:56 +0100

    Updated scripts for renaming of /less directory to /app-less

--- bash/nimzo/nimzo-compile-less.sh
+++ bash/nimzo/nimzo-compile-less.sh
@@ -3,4 +3,4 @@
 # Re-Compiles Less file.
 
 cd ~/Repos/Nimzo/httpdocs
-lessc public/dev/lib/less/app.less > public/min/lib/css/app.min.css
+lessc public/dev/lib/app-less/app.less > public/min/lib/css/app.min.css
--- bash/scripts/update-sleek.sh
+++ bash/scripts/update-sleek.sh
@@ -6,7 +6,7 @@
 cd ~/Repos/Nimzo/httpdocs/private/lib/sleek/
 nimzoSleekPhpLMT=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
 
-cd ~/Repos/Nimzo/httpdocs/public/dev/lib/less/sleek/
+cd ~/Repos/Nimzo/httpdocs/public/dev/lib/app-less/sleek/
 nimzoSleekDevLMT=$(find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -1 | cut -f 1 -d" ")
 
 cd ~/Repos/Sleek/httpdocs/
