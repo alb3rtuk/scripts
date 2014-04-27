b55dd9875a10deea580ed838667baffb1bb38186
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-27 19:29:42 +0100

    Sleek Test Generator bash kick-off script does a quick de-tour to RUBY for some validation :)

--- bash/nimzo/nimzo-generate-sleek-test.sh
+++ bash/nimzo/nimzo-generate-sleek-test.sh
@@ -3,3 +3,25 @@
 . ~/Repos/Scripts/bash/common/utilities.sh
 
 # Script to kick-off my Nimzo test (boiler plate) generator for Sleek.
+
+file=$1
+if [[ ${file} == "" ]]; then
+    echo
+    message red "ERROR" "You must specify a \033[35m'sleek classname'\033[0m. Valid parameters are:"
+    echo
+    echo "        \033[33mSleekDiv\033[0m"
+    echo "        \033[33mSleekDiv.php\033[0m"
+    echo
+    exit
+fi
+
+# Do some quick validation in Ruby..
+ruby ~/Repos/Scripts/ruby/nimzo/nimzo-generate-sleek-test.rb ${file}
+
+# If the Ruby validation passes, continue to PHP.
+cd ~/Repos/Nimzo/tests-php/bin
+echo
+message magenta "SLEEK" "PHPUnit Test Generator"
+echo
+php -f PHPUnit_SleekTestGenerator.php ${file}
+echo
