8eeae8c3ea3a7b32c1c19a668589000eaedcef93
Albert Rannetsperger <alb3rtuk@me.com>
2014-04-29 17:30:24 +0100

    Updated less compile script to have optional minimization.

--- bash/nimzo/nimzo-compile-less.sh
+++ bash/nimzo/nimzo-compile-less.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 # Re-Compiles Less file.
+# If a parameter is passed (ANY PARAMETER) it will minimize the .less, otherwise not.
+
+min=$1
 
 cd ~/Repos/Nimzo/httpdocs
-lessc public/dev/lib/app-less/app.less > public/min/lib/css/app.min.css -x
+
+if [[ ${min} == "" ]]; then
+    lessc public/dev/lib/app-less/app.less > public/min/lib/css/app.min.css
+else
+    lessc public/dev/lib/app-less/app.less > public/min/lib/css/app.min.css -x
+fi
+
