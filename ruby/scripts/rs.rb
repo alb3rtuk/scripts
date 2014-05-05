eb8345479fa4b68399552c91cd01a147947907aa
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-05 14:28:56 +0100

    Quick script to display all available AppScript commands. This may come in handy one day.

--- ruby/scripts/rs.rb
+++ ruby/scripts/rs.rb
@@ -1,1 +1,7 @@
 require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
+require 'rubygems'
+require 'appscript'
+
+# Display list of system commands that can be run.
+systemEvenets = Appscript.app('System Events')
+systemEvenets.methods.each { |method| puts method }
