9fa8fe672f54d325d3709489d5a620cf121ba5f4
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-05 14:29:14 +0100

    Much simpler way of kicking off ruby test-suite.

--- bash/nimzo/nimzo-run-ruby-tests.sh
+++ bash/nimzo/nimzo-run-ruby-tests.sh
@@ -2,12 +2,5 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
-cd ~/Repos/Nimzo-Ruby/tests/
-
-clear
-
-echo
-message magenta "RUBY-RSPEC" "Initializing tests... \033[33m$(date)\033[0m"
-echo
-
-rspec
+cd ~/Repos/Nimzo-Ruby/tests
+ruby TestRunner.rb $1
