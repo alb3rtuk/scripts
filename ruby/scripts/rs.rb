e66e2abb9b982cb9268ea05a4478bb2cde90bec0
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-05 15:31:51 +0100

    Moved handy code into RS file (for now).

--- ruby/scripts/rs.rb
+++ ruby/scripts/rs.rb
@@ -2,6 +2,11 @@ require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
 require 'rubygems'
 require 'appscript'
 
+include Appscript
+
 # Display list of system commands that can be run.
 systemEvenets = Appscript.app('System Events')
 systemEvenets.methods.each { |method| puts method }
+
+# This is how you would run Command + K (in Terminal)
+app('System Events').key_code(40, :using => [:command_down])
