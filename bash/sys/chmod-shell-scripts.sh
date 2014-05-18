44279d9ad29edf418060277637705d0ae4510a33
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-18 18:35:25 +0100

    Updated update-bash-profile script to include nimzo scripts as well.

--- bash/sys/chmod-shell-scripts.sh
+++ bash/sys/chmod-shell-scripts.sh
@@ -5,9 +5,11 @@
 # Finds all bash scripts within ~/Repos/Scripts/ (files ending in .sh) and
 # CHMOD's them to u+x to make them executable.
 
+echo
 cd ~/Repos/Scripts/bash
+find . -type f -name "*.sh" -exec echo "chmod 0755 {}" \; -exec chmod 0755 {} \;
+cd ~/Repos/Nimzo-Ruby/scripts/bash
+find . -type f -name "*.sh" -exec echo "chmod 0755 {}" \; -exec chmod 0755 {} \;
 echo
-find . -type f -name *.sh -exec echo "chmod 0755 {}" \; -exec chmod 0755 {} \;
-echo
-message green "SUCCESS" "All shell scripts within ~/Repos/Scripts/bash/ have been chmod'ed to 0755"
+message green "SUCCESS" "All shell scripts have been chmod'ed to 0755"
 echo
