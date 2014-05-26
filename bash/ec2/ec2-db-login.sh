dde3f37a8c20d94d1d90e0669914bbf1d0038059
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-27 00:55:04 +0100

    Temporary login scripts to EC2.

--- bash/ec2/ec2-db-login.sh
+++ bash/ec2/ec2-db-login.sh
@@ -2,4 +2,8 @@
 
 . /Users/Albert/Repos/Scripts/.secrets/secrets.sh
 
-mysql -h ${EC2_MYSQL_HOST} -P 3306 -u root -p
+#time mysql -h ${EC2_MYSQL_HOST} -P 3306 -u root -p${EC2_MYSQL_PASS} -e 'quit'
+#time mysql -h nimzo-test.cr27trwznlei.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppassword -e 'quit'
+#time mysql -h www.brooklins.com -P 3306 --skip-secure-auth -u brookdata -ptkmdgqps52 -e 'quit'
+
+mysql -h ${EC2_MYSQL_HOST} -P 3306 -u ${EC2_MYSQL_USER} -p${EC2_MYSQL_PASS}
--- bash/ec2/ec2-login.sh
+++ bash/ec2/ec2-login.sh
@@ -5,10 +5,12 @@
 
 cd ~/Repos/Chef/
 
-region=$1
-initializeRegion
+#region=$1
+#initializeRegion
+#
+#ssh_target=$2
+#validateSSHTarget
+#
+#ssh -i ${pem_file} ${ssh_user}@${ssh_target}
 
-ssh_target=$2
-validateSSHTarget
-
-ssh -i ${pem_file} ${ssh_user}@${ssh_target}
+ssh -i /Users/Albert/.ssh/ec2-eu-west-1.pem ec2-user@ec2-54-76-49-176.eu-west-1.compute.amazonaws.com
