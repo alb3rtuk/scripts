6d2449f73ae90b256c7b499b3db386a106193c0b
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-30 00:26:33 +0100

    Updated repo pusher script to push ALL MY repos.

--- bash/launchd/albert.git.push.repos.sh
+++ bash/launchd/albert.git.push.repos.sh
@@ -2,19 +2,34 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
+cd ~/Repos/Chef/
+git push > /dev/null 2>&1
+
+cd ~/Repos/Raillercoaster/
+git push > /dev/null 2>&1
+
+cd ~/Repos/Scripts/
+git push > /dev/null 2>&1
+
+cd ~/Repos/brooklins-legacy/
+git push > /dev/null 2>&1
+
+cd ~/Repos/nimzo-java/
+git push > /dev/null 2>&1
+
+cd ~/Repos/nimzo-legacy/
+git push > /dev/null 2>&1
+
 cd ~/Repos/nimzo-php/
 git push > /dev/null 2>&1
 
 cd ~/Repos/nimzo-ruby/
 git push > /dev/null 2>&1
 
-cd ~/Repos/Nimzo-Java/
+cd ~/Repos/sleek/
 git push > /dev/null 2>&1
 
-cd ~/Repos/Scripts/
-git push > /dev/null 2>&1
-
-cd ~/Repos/Sleek/
+cd ~/Repos/sleek-furniture/
 git push > /dev/null 2>&1
 
 exit
