2c5decd651dccf0c730995b94c7f1154d11751b7
Albert Rannetsperger <alb3rtuk@me.com>
2014-05-29 22:52:42 +0100

    Updated bash_profile and update script to pull aliases from the ruby repo to avoid having to enter them twice everytime.

--- bash/scripts/update-bash-profile.sh
+++ bash/scripts/update-bash-profile.sh
@@ -4,9 +4,23 @@
 # Scripts/backup/.bash_profile
 
 . ~/Repos/Scripts/bash/common/utilities.sh
+
+if [[ ! -f ~/Repos/nimzo-ruby/config/.bashrc_aliases ]]; then
+    echo
+    message red "ERROR" "Cannot find the following file: \033[33m~/Repos/nimzo-ruby/config/.bashrc_aliases\033[0m"
+    echo
+    exit
+fi
+
 . ~/Repos/Scripts/bash/sys/chmod-shell-scripts.sh
 
-cp ~/Repos/Scripts/backup/.bash_profile ~/.bash_profile
+# Include the NIMZO aliases.
+bash_profile=`cat ~/Repos/Scripts/backup/.bash_profile`
+bashrc_aliases=`cat ~/Repos/nimzo-ruby/config/.bashrc_aliases`
+find=">>> NIMZO ALIASES <<<"
+bash_final=${bash_profile//${find}/${bashrc_aliases}}
+
+echo "${bash_final}" > ~/.bash_profile
 
 message green "SUCCESS" "Bash Profile has been updated. Don't forget to run $ `tput setab 3` . ~/.bash_profile `tput setab 0` or $ `tput setab 3` sb `tput setab 0`"
 echo
