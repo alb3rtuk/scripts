5a3d4f2b0bce87bc3a09ef69198d5cede9b96bbf
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-06-01 20:06:48 +0100

    New script to kick off 'update' script on EC2 instances.

--- bash/ec2/ec2-server-update.sh
+++ bash/ec2/ec2-server-update.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 
+. ~/Repos/Scripts/bash/common/utilities.sh
+. ~/Repos/Scripts/.secrets/secrets_pem.sh
+. ~/Repos/Scripts/.secrets/server_host.sh
+. ~/Repos/Scripts/.secrets/server_user.sh
+
+echo
+message green "EC2" "Running 'update' script on EC2 instance.. \033[33m${EC2_SERVER_HOST}\033[0m"
+echo
+
+ssh -l ${EC2_SERVER_USER} -i ${PEM_EU_WEST_1} ${EC2_SERVER_HOST} 'cd ~/Repos/nimzo-ruby/scripts/server; git pull; ./server-update.sh;'
