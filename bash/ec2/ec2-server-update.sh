a7ae5aab1d37dc505b6dd43d590a0457203eab6e
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-06-02 01:26:38 +0100

    Improvements to EC2 update script.

--- bash/ec2/ec2-server-update.sh
+++ bash/ec2/ec2-server-update.sh
@@ -9,4 +9,24 @@ echo
 message green "EC2" "Running 'update' script on EC2 instance.. \033[33m${EC2_SERVER_HOST}\033[0m"
 echo
 
-ssh -l ${EC2_SERVER_USER} -i ${PEM_EU_WEST_1} ${EC2_SERVER_HOST} 'cd ~/Repos/nimzo-ruby/scripts/server; git pull; ./server-update.sh;'
+cd ~/Repos/nimzo-ruby/
+git push
+
+ssh -t -l ${EC2_SERVER_USER} -i ${PEM_EU_WEST_1} ${EC2_SERVER_HOST} '
+cd ~/Repos/nimzo-ruby/scripts/server;
+git pull;
+./server-update.sh;
+rm -rf ~/Repos/nimzo-ruby/.secrets;
+mkdir ~/Repos/nimzo-ruby/.secrets;
+rm -rf ~/Repos/nimzo-php/httpdocs/.secrets;
+mkdir ~/Repos/nimzo-php/httpdocs/.secrets;
+'
+
+# COPY SECRETS TO SERVER
+scp -i ${PEM_EU_WEST_1} -r ~/Repos/nimzo-ruby/.secrets/* ${EC2_SERVER_USER}@${EC2_SERVER_HOST}:~/Repos/nimzo-ruby/.secrets
+scp -i ${PEM_EU_WEST_1} -r ~/Repos/nimzo-php/httpdocs/.secrets/* ${EC2_SERVER_USER}@${EC2_SERVER_HOST}:~/Repos/nimzo-php/httpdocs/.secrets
+scp -i ${PEM_EU_WEST_1} -r ~/Repos/nimzo-java/ebay-service/src/main/resources/eBayAppConfig.properties ${EC2_SERVER_USER}@${EC2_SERVER_HOST}:~/Repos/nimzo-java/ebay-service/src/main/resources/eBayAppConfig.properties
+
+echo
+message green "EC2" "Update complete!"
+echo
