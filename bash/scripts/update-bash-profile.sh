cb5a273754d4560bccdb6384ad6632d7134ff6ad
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-06-03 14:23:38 +0100

    Changed name of shared .bashrc file in other repo.

--- bash/scripts/update-bash-profile.sh
+++ bash/scripts/update-bash-profile.sh
@@ -5,9 +5,9 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
-if [[ ! -f ~/Repos/nimzo-ruby/config/.bashrc_aliases ]]; then
+if [[ ! -f ~/Repos/nimzo-ruby/config/.bashrc_shared ]]; then
     echo
-    message red "ERROR" "Cannot find the following file: \033[33m~/Repos/nimzo-ruby/config/.bashrc_aliases\033[0m"
+    message red "ERROR" "Cannot find the following file: \033[33m~/Repos/nimzo-ruby/config/.bashrc_shared\033[0m"
     echo
     exit
 fi
@@ -16,7 +16,7 @@ fi
 
 # Include the NIMZO aliases.
 bash_profile=`cat ~/Repos/Scripts/backup/.bash_profile`
-bashrc_aliases=`cat ~/Repos/nimzo-ruby/config/.bashrc_aliases`
+bashrc_aliases=`cat ~/Repos/nimzo-ruby/config/.bashrc_shared`
 find=">>> NIMZO ALIASES <<<"
 bash_final=${bash_profile//${find}/${bashrc_aliases}}
 
