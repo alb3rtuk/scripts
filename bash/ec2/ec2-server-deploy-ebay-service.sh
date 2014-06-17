12db632203936fbaae391b32205f9c59b4ec0b7f
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-06-17 11:59:54 +0100

    Updated ebay-service.war deployment script.

--- bash/ec2/ec2-server-deploy-ebay-service.sh
+++ bash/ec2/ec2-server-deploy-ebay-service.sh
@@ -11,10 +11,15 @@ echo
 
 scp -i ${PEM_EU_WEST_1} -r ~/Repos/nimzo-java/ebay-service/target/ebay-service.war ${EC2_SERVER_USER}@${EC2_SERVER_HOST}:/tmp/
 
-#ssh -t -l ${EC2_SERVER_USER} -i ${PEM_EU_WEST_1} ${EC2_SERVER_HOST} '
-#cd $HOME/Repos/nimzo-ruby/scripts/server;
-#./server-update-php.sh;
-#'
+ssh -t -l ${EC2_SERVER_USER} -i ${PEM_EU_WEST_1} ${EC2_SERVER_HOST} '
+cd /usr/local/tomcat/bin
+./shutdown.sh
+rm -rf /usr/local/tomcat/webapps/ebay-service;
+mkdir /usr/local/tomcat/webapps/ebay-service
+mv /tmp/ebay-service.war /usr/local/tomcat/webapps/ebay-service/ebay-service.war
+cd /usr/local/tomcat/bin
+./startup.sh
+'
 
 echo
 message green "EC2" "Deployment complete!"
