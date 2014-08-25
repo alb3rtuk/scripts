ff23941b362fbca704c6a06c06e6a32c36bcf59a
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-08-25 01:41:38 +0100

    Created new database models (Ruy Lopez) for my own personal stuff.

---
+++ mysql/ruy_lopez_update_script.sql
@@ -0,0 +1,23 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`bank_account`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`bank_account` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`bank_account` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
