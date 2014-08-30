8e962ad0f8b2cc2aa8baae445f259b24ef34ae5e
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-08-30 18:15:10 +0100

    Minor changes to the way it initializes from bash.

--- bash/scripts/lotto-predicter.sh
+++ bash/scripts/lotto-predicter.sh
@@ -8,4 +8,4 @@ echo
 message magenta "LOTTO PREDICTER" " Attempting to predict the next lottery numbers... \033[33m$(date)\033[0m"
 echo
 
-ruby ~/Repos/Scripts/ruby/scripts/lotto-predicter.rb
+ruby ~/Repos/Scripts/ruby/scripts/lotto-predicter.rb $1
--- ruby/scripts/lotto-predicter.rb
+++ ruby/scripts/lotto-predicter.rb
@@ -2,4 +2,4 @@ require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
 require '/Users/Albert/Repos/Scripts/ruby/lib/encrypter.rb'
 require '/Users/Albert/Repos/Scripts/ruby/lib/selenium/lotto-predicter.rb'
 
-LottoPredicter.new.runPrediction
+LottoPredicter.new(ARGV[0])
