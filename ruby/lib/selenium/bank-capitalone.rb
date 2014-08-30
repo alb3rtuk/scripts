9bf5a52f69ee97379adec81ab9d3590906b5df3e
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-08-30 02:02:33 +0100

    Capital One correctly retrieving transactions.

--- ruby/lib/selenium/bank-capitalone.rb
+++ ruby/lib/selenium/bank-capitalone.rb
@@ -3,13 +3,14 @@ require '/Users/Albert/Repos/Scripts/ruby/lib/utilities.rb'
 class BankCapitalOne
     include CommandLineReporter
 
-    def initialize(username, security, displays = 'single', headless = false, displayProgress = false)
+    def initialize(username, security, displays = 'single', headless = false, displayProgress = false, databaseConnection = nil)
         @username = username
         @security = security
         @displays = displays
         @headless = headless
         @displayProgress = displayProgress
         @login_uri = 'https://www.capitaloneonline.co.uk/CapitalOne_Consumer/Login.do'
+        @databaseConnection = databaseConnection
     end
 
     # Gets you as far as CapitalOne account overview screen & then returns the browser for (possible) further manipulation.
@@ -36,7 +37,49 @@ class BankCapitalOne
         if @displayProgress
             puts "\x1B[90mSuccessfully logged in to CapitalOne\x1B[0m\n"
         end
-        return browser
+        browser
+    end
+
+    def runExtraction(showInTerminal = false)
+        attempt = 0
+        succeeded = false
+        while !succeeded
+            begin
+                attempt = attempt + 1
+                data = getAllData(showInTerminal)
+                data = data[1]
+            rescue Exception => e
+                succeeded = false
+                if showInTerminal
+                    puts "\x1B[31mAttempt #{attempt} failed with message: \x1B[90m#{e.message}.\x1B[0m"
+                    # puts e.backtrace
+                end
+            else
+                succeeded = true
+            ensure
+                if succeeded
+
+                    @databaseConnection.query("INSERT INTO bank_account_type_credit_card (bank_account_id, balance, balance_available, balance_limit, date_fetched, date_fetched_string, minimum_payment, minimum_payment_date) VALUES (10, #{data['balance']}, #{data['available_funds']}, #{data['credit_limit']}, '#{DateTime.now}', '#{DateTime.now}', #{data['minimum_payment']}, '#{data['due_date']}')")
+                    BankCommon.new.insertTransactions(@databaseConnection, data['main_card'], 10)
+                    # Check if existing transactions (in last month) still exist
+                    objectData = Array[
+                        {:bank_account_id => 10, :transactions => data['main_card']},
+                    ]
+                    BankCommon.new.checkIfTransactionStillExist(@databaseConnection, objectData)
+                    if showInTerminal
+                        puts "\x1B[32mSuccess (Capital One)\x1B[0m"
+                    end
+
+                else
+                    if attempt >= 1
+                        succeeded = true
+                        if showInTerminal
+                            puts "\x1B[31mSite is either down or there is an error in the Capital One script.\x1B[0m"
+                        end
+                    end
+                end
+            end
+        end
     end
 
     def getBalances(showInTerminal = false, browser = self.login)
@@ -45,7 +88,7 @@ class BankCapitalOne
         data['available_funds'] = browser.td(:text, /Available to spend/).parent.cell(:index => 1).text.delete('£').delete(',').to_f
         data['credit_limit'] = browser.td(:text, /Credit limit/).parent.cell(:index => 1).text.delete('£').delete(',').to_f
         data['minimum_payment'] = browser.td(:text, /Minimum payment/).parent.cell(:index => 1).text.delete('£').delete(',').to_f
-        data['due_date'] = DateTime.strptime(browser.td(:text, /Payment due date/).parent.cell(:index => 1).text, '%d-%m-%Y')
+        data['due_date'] = DateTime.strptime(browser.td(:text, /Payment due date/).parent.cell(:index => 1).text, '%d-%m-%Y').strftime('%Y-%m-%d')
         if showInTerminal
             puts "\n[ #{Rainbow('CapitalOne').foreground('#ff008a')} ]"
             table(:border => true) do
@@ -66,6 +109,93 @@ class BankCapitalOne
             end
         end
         Array[browser, data]
+    end
+
+    def getAllData(showInTerminal = false, browser = self.login)
+        data_main_card = Array.new
+
+        # Get balances first
+        data = getBalances(false, browser)
+        data = data[1]
+
+        if showInTerminal
+            puts "\x1B[90mSuccessfully retrieved balances\x1B[0m"
+        end
+
+        browser.input(:type => 'submit', :value => 'VIEW ALL TRANSACTIONS').click
+
+        pageIndex = 0
+        while pageIndex <= 5
+            browser.select_list(:name => 'cycleDate').option(:index => pageIndex).select
+            browser.input(:type => 'submit', :value => 'VIEW TRANSACTIONS').click
+            if browser.div(:class => 'chart').table.exists?
+                data_main_card.push(*getTransactionsFromTable(browser.div(:class => 'chart').table))
+            end
+            pageIndex = pageIndex + 1
+        end
+
+        # Add transactions to final array
+        data['main_card'] = data_main_card
+
+        Array[browser, data]
+    end
+
+    # Takes table and gets transactions from that.
+    def getTransactionsFromTable(table)
+        rowCount = 0
+        transactions = Array.new
+        table.rows.each do |tableRow|
+            rowCount = rowCount + 1
+            if rowCount <= 1
+                next
+            end
+            rowData = {}
+            cellCount = 0
+            tableRow.cells.each do |tableCell|
+
+                # If no transactions, break out of loop immediately.
+                if tableCell.text.include? 'There were no transactions for the selected period.'
+                    break;
+                end
+
+                cellCount = cellCount + 1
+                if cellCount == 1
+                    rowData['date'] = tableCell.text
+                elsif cellCount == 2
+                    rowData['description'] = tableCell.text
+                elsif cellCount == 3
+                    rowData['paid_out'] = tableCell.text
+                elsif cellCount == 4
+                    rowData['paid_in'] = tableCell.text
+                end
+            end
+            unless rowData.empty?
+                transactions << rowData
+            end
+        end
+        sanitizeTransactions(transactions)
+    end
+
+    # Takes transaction data and sanitizes it.
+    def sanitizeTransactions(transactions)
+        sanitizedArray = Array.new
+        unless transactions.empty?
+            transactions.each do |transaction|
+                newData = {}
+                # Date
+                newData['date'] = DateTime.strptime(transaction['date'], '%d-%m-%Y').strftime('%Y-%m-%d')
+                # Description
+                newData['description'] = transaction['description']
+                # Paid In/Out
+                newData['paid_in'] = transaction['paid_in'].delete(',').delete('£').to_f
+                newData['paid_out'] = transaction['paid_out'].delete(',').delete('£').to_f
+                # DUD INFO
+                newData['type'] = (newData['paid_in'] > 0) ? 'CR' : 'CC'
+                newData['balance'] = 0
+                sanitizedArray << newData
+            end
+        end
+        sanitizedArray
     end
 
 end
