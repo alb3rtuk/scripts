e7e80edd1b3496fe099d031413f957cbfecb15ac
Albert Rannetsperger <alb3rtuk@hotmail.com>
2014-09-07 01:53:03 +0100

    Reduced the number of repos I'm actively commiting to.

--- bash/git/git-status-all.sh
+++ bash/git/git-status-all.sh
@@ -7,21 +7,12 @@ echo
 message magenta " GIT " "Getting status of all repos... \033[33m$(date +"%a %e %b %Y %H:%M:%S")\033[0m"
 echo
 
-
-echo "\033[35m~/Repos/Chef/\033[0m"
+echo "\033[35m~/Repos/chef/\033[0m"
 cd ~/Repos/Chef/
 git status
 
-echo "\033[35m~/Repos/Raillercoaster/\033[0m"
-cd ~/Repos/Raillercoaster/
-git status
-
-echo "\033[35m~/Repos/Scripts/\033[0m"
+echo "\033[35m~/Repos/scripts/\033[0m"
 cd ~/Repos/Scripts/
-git status
-
-echo "\033[35m~/Repos/brooklins-legacy/\033[0m"
-cd ~/Repos/brooklins-legacy/
 git status
 
 echo "\033[35m~/Repos/nimzo-java/\033[0m"
@@ -40,14 +31,4 @@ echo "\033[35m~/Repos/nimzo-ruby/\033[0m"
 cd ~/Repos/nimzo-ruby/
 git status
 
-echo "\033[35m~/Repos/sleek/\033[0m"
-cd ~/Repos/sleek/
-git status
-
-echo "\033[35m~/Repos/sleek-furniture/\033[0m"
-cd ~/Repos/sleek-furniture/
-git status
-
 echo
-
-exit
--- bash/git/git-sync-all.sh
+++ bash/git/git-sync-all.sh
@@ -7,23 +7,13 @@ echo
 message magenta " GIT " "Syncing all repos... \033[33m$(date +"%a %e %b %Y %H:%M:%S")\033[0m"
 echo
 
-echo "\033[35m~/Repos/Chef/\033[0m"
+echo "\033[35m~/Repos/chef/\033[0m"
 cd ~/Repos/Chef/
 git pull
 git push
 
-echo "\033[35m~/Repos/Raillercoaster/\033[0m"
-cd ~/Repos/Raillercoaster/
-git pull
-git push
-
-echo "\033[35m~/Repos/Scripts/\033[0m"
+echo "\033[35m~/Repos/scripts/\033[0m"
 cd ~/Repos/Scripts/
-git pull
-git push
-
-echo "\033[35m~/Repos/brooklins-legacy/\033[0m"
-cd ~/Repos/brooklins-legacy/
 git pull
 git push
 
@@ -47,16 +37,4 @@ cd ~/Repos/nimzo-ruby/
 git pull
 git push
 
-echo "\033[35m~/Repos/sleek/\033[0m"
-cd ~/Repos/sleek/
-git pull
-git push
-
-echo "\033[35m~/Repos/sleek-furniture/\033[0m"
-cd ~/Repos/sleek-furniture/
-git pull
-git push
-
 echo
-
-exit
