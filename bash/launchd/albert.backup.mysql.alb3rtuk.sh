2f141700837bc593504939178692320eecf31069
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2014-09-10 22:42:26 +0100

    Minor changes to script which backs up SQL DBs to repo.

--- bash/launchd/albert.backup.mysql.alb3rtuk.sh
+++ bash/launchd/albert.backup.mysql.alb3rtuk.sh
@@ -7,4 +7,4 @@ logCron "Attempting to backup/dump 'alb3rtuk' EC2 MySQL DB to local repo."
 
 date="$(date +%Y-%m-%d)"
 
-mysqldump -h ${EC2_MYSQL_ALB3RTUK_HOST} -P 3306 -u ${EC2_MYSQL_ALB3RTUK_ROOT_USER} -p${EC2_MYSQL_ALB3RTUK_ROOT_PASS} ${EC2_MYSQL_ALB3RTUK_DEFAULT_SCHEMA} > $HOME/Repos/SQL-Backups/alb3rtuk/alb3rtuk-${date}.sql
+mysqldump -h ${EC2_MYSQL_ALB3RTUK_HOST} -P 3306 -u ${EC2_MYSQL_ALB3RTUK_ROOT_USER} -p${EC2_MYSQL_ALB3RTUK_ROOT_PASS} ${EC2_MYSQL_ALB3RTUK_DEFAULT_SCHEMA} > $HOME/Repos/sql-backups/alb3rtuk/alb3rtuk-${date}.sql
