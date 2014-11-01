6d23abfde73b42cb61a91f7f3e18b380bd179139
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2014-11-01 16:27:56 +0000

    Can now build 'convoy' ruby gem using the same build script for 'brightpearl-cli'.

--- bash/development/gem-build.sh
+++ bash/development/gem-build.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-# Build the 'brightpearl-cli' ruby gem.
+# Build the 'brightpearl-cli' ruby gem (or 'convoy' gem when called with ANY argument).
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
-cd ~/Repos/brightpearl-cli/
+ARG=$1
 
-sudo gem build brightpearl-cli.gemspec
-sudo gem install brightpearl-cli-1.0.0.gem
+if [[ ${ARG} != '' ]]; then
+
+    cd ~/Repos/convoy/
+    sudo gem build convoy.gemspec
+    sudo gem install convoy-1.0.0.gem
+
+else
+
+    cd ~/Repos/brightpearl-cli/
+    sudo gem build brightpearl-cli.gemspec
+    sudo gem install brightpearl-cli-1.0.0.gem
+
+fi
+
+exit
