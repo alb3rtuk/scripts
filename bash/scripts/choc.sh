de7b7d69220b60b1d5ced84ee82b96733a8ea979
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2014-11-01 11:08:43 +0000

    Updated alias scripts for chocolate and textmate. Added comments.

--- bash/scripts/choc.sh
+++ bash/scripts/choc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Open a file in Chocolat (IE: $ choc <filename>)
+
 file=$1
 
 open -a chocolat $1
---
+++ bash/scripts/mate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Open a file in Textmate (IE: $ textmate <filename>)
+
+file=$1
+
+open -a textmate $1
