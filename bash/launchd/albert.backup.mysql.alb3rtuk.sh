9b2ee29b0c311c251478af405d7688341fce7e69
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2014-11-02 14:36:26 +0000

    Fixed SQL backup script.. somehow?? lol

--- bash/launchd/albert.backup.mysql.alb3rtuk.sh
+++ bash/launchd/albert.backup.mysql.alb3rtuk.sh
@@ -7,4 +7,6 @@ logCron "Attempting to backup/dump 'alb3rtuk' EC2 MySQL DB to local repo."
 
 date="$(date +%Y-%m-%d)"
 
-mysqldump -h ${EC2_MYSQL_ALB3RTUK_HOST} -P 3306 -u ${EC2_MYSQL_ALB3RTUK_ROOT_USER} -p${EC2_MYSQL_ALB3RTUK_ROOT_PASS} ${EC2_MYSQL_ALB3RTUK_DEFAULT_SCHEMA} > $HOME/Repos/sql-backups/alb3rtuk/alb3rtuk-${date}.sql
+mysqldump -u ${EC2_MYSQL_ALB3RTUK_ROOT_USER} -p${EC2_MYSQL_ALB3RTUK_ROOT_PASS} -P 3306 -h ${EC2_MYSQL_ALB3RTUK_HOST} ${EC2_MYSQL_ALB3RTUK_DEFAULT_SCHEMA} > $HOME/Repos/sql-backups/alb3rtuk/alb3rtuk-${date}.sql
+
+exit
