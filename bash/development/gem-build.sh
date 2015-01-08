b47a086e01b7b0c52bb92bba03f4e4a93b4b0e45
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2015-01-08 10:55:48 +0000

    Another attempt to make compiling ruby gems faster. Maybe faster now.

--- bash/development/gem-build.sh
+++ bash/development/gem-build.sh
@@ -9,28 +9,32 @@ ARG=$1
 if [[ ${ARG} == 'brightpearl-cli' || ${ARG} == '' ]]; then
 
     # RubyGem Documentation:
+    #
     # http://guides.rubygems.org/command-reference/#gem-install
+    #
+    # -p - Use HTTP proxy for remote operations
+    # -l - Restrict operations to the LOCAL domain
+    # -N - Disable documentation generation
+    # -V - Verbose
+    # -f - Force gem to install, bypassing dependency checks
+    # --backtrace -- Show stack backtrace on errors
 
     cd ~/Repos/brightpearl-cli/
     sudo gem build brightpearl-cli.gemspec
-    sudo gem install brightpearl-cli-1.0.0.gem --backtrace / # Show stack backtrace on errors
-        -p / # Use HTTP proxy for remote operations
-        -l / # Restrict operations to the LOCAL domain
-        -N / # Disable documentation generation
-        -V / # Verbose
-        -f   # Force gem to install, bypassing dependency checks
+    sudo gem install brightpearl-cli-1.0.0.gem -p -l -N -V -f --backtrace
+
 
 elif [[ ${ARG} == 'columnist' ]]; then
 
     cd ~/Repos/columnist/
     sudo gem build columnist.gemspec
-    sudo gem install columnist-1.0.0.gem
+    sudo gem install columnist-1.0.0.gem -p -l -N -V -f --backtrace
 
 elif [[ ${ARG} == 'convoy' ]]; then
 
     cd ~/Repos/convoy/
     sudo gem build convoy.gemspec
-    sudo gem install convoy-1.0.0.gem
+    sudo gem install convoy-1.0.0.gem -p -l -N -V -f --backtrace
 
 else
 
