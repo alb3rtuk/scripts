c40a5217a108a9260c7a7c24734864430f8d4e92
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2015-01-17 11:45:41 +0000

    Minor alterations to my beloved Encrytper.

--- ruby/lib/encrypter.rb
+++ ruby/lib/encrypter.rb
@@ -1,36 +1,36 @@
-require 'digest/sha1'
 require 'base64'
 
 class Encrypter
 
-    # I keep all my keys, hex's & passwords stored (and encrypted) locally.
-    # These will never be transmitted across the web, git, or anything.. ever!
-    # If you made it far enough to read this comment then you, sir, are a sneaky dog.. and I salute you :)
     def initialize
+
         @key = $cryptoKey
         @hex = $cryptoHex
         self
+
     end
 
-    # Encrypts a string. Please note, this is not bomb-proof.
     def encrypt(string)
-        c = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+
+        c = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
         c.encrypt
         c.key = @key = Digest::SHA1.hexdigest(@hex)
         e = c.update(string)
         e << c.final
         Base64.encode64(e)
+
     end
 
-    # Decrypts a string.
     def decrypt(string)
+
         string = Base64.decode64(string)
-        c = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+        c = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
         c.decrypt
         c.key = @key = Digest::SHA1.hexdigest(@hex)
         d = c.update(string)
         d << c.final
         d
+
     end
 
 end
