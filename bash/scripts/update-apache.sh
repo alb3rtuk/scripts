1b91dd58f0910ac2f9daf9d69f97345b857612a4
Albert Rannetsperger <alb3rtuk@hotmail.com>
2015-06-29 12:11:22 +0100

    Added /etc/exports to 'update-apache' script.

--- bash/scripts/update-apache.sh
+++ bash/scripts/update-apache.sh
@@ -5,6 +5,9 @@
 sudo cp ~/Repos/Scripts/backup/hosts /private/etc/hosts
 message green "SUCCESS" "Updated: \033[33m/private/etc/hosts\033[0m"
 
+sudo cp ~/Repos/Scripts/backup/exports /private/etc/exports
+message green "SUCCESS" "Updated: \033[33m/private/etc/exports\033[0m"
+
 sudo cp ~/Repos/Scripts/backup/httpd.conf /private/etc/apache2/httpd.conf
 message green "SUCCESS" "Updated: \033[33m/private/etc/apache2/httpd.conf\033[0m"
 
@@ -16,6 +19,9 @@ message green "SUCCESS" "Updated: \033[33m/private/etc/php.ini\033[0m"
 
 sudo cp ~/Repos/Scripts/backup/hosts /etc/hosts
 message green "SUCCESS" "Updated: \033[33m/etc/hosts\033[0m"
+
+sudo nfsd restart
+message magenta "SUCCESS" "Restarted NFS"
 
 sudo apachectl restart
 message magenta "SUCCESS" "Restarted Apache"
