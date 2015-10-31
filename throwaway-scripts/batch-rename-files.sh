e5a8c912e1f00a08faae250826a03eda122ad7fc
Albert Rannetsperger <alb3rtuk@hotmail.com>
2015-10-31 15:05:31 +0000

    Forgot URL for reference.

--- throwaway-scripts/batch-rename-files.sh
+++ throwaway-scripts/batch-rename-files.sh
@@ -2,6 +2,8 @@
 
 cd ~/Repos/nimzo-java/nimzo-lib/src/main/resources/assets-test/xml-ebay
 
+# http://stackoverflow.com/questions/1224766/how-do-i-rename-the-extension-for-a-batch-of-files
+
 for file in *.txt.xml; do
     echo ${file}
     mv "$file" "`basename ${file} .txt.xml`.xml"
