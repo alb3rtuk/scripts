536de366cb12a8c6458cef619d6528d2ee85f9d7
Albert Rannetsperger <alb3rtuk@hotmail.com>
2015-11-08 12:39:49 +0000

    File rename script converted to do eBay XML files.

--- throwaway-scripts/batch-rename-files.sh
+++ throwaway-scripts/batch-rename-files.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cd ~/Repos/nimzo-java/nimzo-lib/src/main/resources/assets-test/xml-ebay
+cd ~/Repos/nimzo-ruby/assets/ebay/
 
 # http://stackoverflow.com/questions/1224766/how-do-i-rename-the-extension-for-a-batch-of-files
 
-for file in *.txt.xml; do
+for file in *.txt; do
     echo ${file}
-    mv "$file" "`basename ${file} .txt.xml`.xml"
+    mv "$file" "`basename ${file} .txt`.xml"
 done
