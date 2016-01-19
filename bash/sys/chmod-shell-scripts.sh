b4d39fbdc05bc789fcce524647da670a7d7afbdb
Albert Rannetsperger <alb3rtuk@hotmail.com>
2016-01-19 09:59:01 +0000

    Nimzo legacy script fix.

--- bash/sys/chmod-shell-scripts.sh
+++ bash/sys/chmod-shell-scripts.sh
@@ -8,7 +8,7 @@
 echo
 cd ~/Repos/scripts/bash
 find . -type f -name "*.sh" -exec echo "chmod 0755 {}" \; -exec chmod 0755 {} \;
-cd ~/Repos/nimzo-ruby/scripts/nimzo
+cd ~/Repos/nimzo-ruby/legacy/scripts/nimzo/
 find . -type f -name "*.sh" -exec echo "chmod 0755 {}" \; -exec chmod 0755 {} \;
 echo
 message green "SUCCESS" "All shell scripts have been chmod'ed to 0755"
