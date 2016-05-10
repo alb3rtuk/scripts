921439e7c2b053f74f16d9baca7f52ccb9b4855f
Albert Rannetsperger <alb3rtuk@hotmail.com>
2016-05-10 11:15:26 -1000

    Modified (and simplified) my 'my' gem build-script.

--- bash/development/gem-build.sh
+++ bash/development/gem-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Build my ruby gems. Specify gem name as argument (or 'brightpearl-cli' by default).
+# Builds 'my' ruby gem.
 
 # RubyGem Documentation:
 #
@@ -15,50 +15,8 @@
 
 . ~/Repos/Scripts/bash/common/utilities.sh
 
-ARG=$1
-
-copy_shared_files() {
-    core=(~/Repos/shared/ruby-core/*)
-    routes=(~/Repos/shared/ruby-routes/*)
-    for ((i=0; i<${#core[@]}; i++)); do
-        cp ${core[$i]} $1
-        echo "cp ${core[$i]} $1"
-    done
-    for ((i=0; i<${#routes[@]}; i++)); do
-        cp ${routes[$i]} $2
-        echo "cp ${routes[$i]} $2"
-    done
-}
-
-remove_shared_files() {
-    core=(~/Repos/shared/ruby-core/*)
-    routes=(~/Repos/shared/ruby-routes/*)
-    for ((i=0; i<${#core[@]}; i++)); do
-        o=${core[$i]}
-        f=~/Repos/shared/ruby-core/
-        r=$1
-        result="${o/$f/$r}"
-        rm ${result}
-        echo "rm ${result}"
-    done
-    for ((i=0; i<${#routes[@]}; i++)); do
-        o=${routes[$i]}
-        f=~/Repos/shared/ruby-routes/
-        r=$2
-        result="${o/$f/$r}"
-        rm ${result}
-        echo "rm ${result}"
-    done
-}
-
 sudo cd ~/Repos/my-cli/
-
-copy_shared_files ~/Repos/my-cli/lib/core/ ~/Repos/my-cli/lib/routes/
-
-cd ~/Repos/my-cli/
 sudo gem build my-cli.gemspec
-sudo gem install my-cli-1.0.0.gem --backtrace -V -l
-
-remove_shared_files ~/Repos/my-cli/lib/core/ ~/Repos/my-cli/lib/routes/
+sudo gem install ./my-cli-1.0.0.gem --backtrace -V -l
 
 exit
