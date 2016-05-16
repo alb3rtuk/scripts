0ae81af96f83c91b090a31502f3fd362bc30f8da
Albert Rannetsperger <alb3rtuk@hotmail.com>
2016-05-16 13:36:49 -1000

    Backup script to build 'nimzo-lib' as a fallback when things go wrong.

---
+++ bash/development/gem-build-nimzo-lib.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Builds 'my' ruby gem.
+
+# RubyGem Documentation:
+#
+# http://guides.rubygems.org/command-reference/#gem-install
+#
+# -p - Use HTTP proxy for remote operations
+# -l - Restrict operations to the LOCAL domain
+# -N - Disable documentation generation
+# -V - Verbose
+# -f - Force gem to install, bypassing dependency checks
+# --backtrace -- Show stack backtrace on errors
+
+. ~/Repos/Scripts/bash/common/utilities.sh
+
+sudo cd ~/Repos/nimzo/nimzo-lib/ && cd ~/Repos/nimzo/nimzo-lib/
+sudo gem build nimzo-lib.gemspec
+sudo gem install ./nimzo-lib-1.0.0.gem --backtrace -V -l
